7e01d3ad0f9d3b203faa05033eb3c73a0c829c22
SaniyaFarooqui <saniyafarooqui05@gmail.com>
2024-04-18 21:51:55 +0530

    prisma setup database setup

--- index.ts
+++ index.ts
@@ -1,14 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import db from "./src/config/database"
 import cors from "cors";
 import bodyParser from "body-parser";
 
-db.sync({alter:true}).then(()=>{
-    console.log("Database is connected successfully")
-}).catch((error)=>{
-    console.log(error);
-})
+
 
 dotenv.config()
 let app = express()
